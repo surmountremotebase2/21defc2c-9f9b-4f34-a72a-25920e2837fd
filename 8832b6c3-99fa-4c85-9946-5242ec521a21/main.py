567cd79125f9d3cb67df9e76188757b94f6894ce
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-20 11:00:49 -0400

    User Ryan Cheng saved changes to multiple files files at 06/20/2024, 15:00:49 UTC time.

--- 8832b6c3-99fa-4c85-9946-5242ec521a21/main.py
+++ 8832b6c3-99fa-4c85-9946-5242ec521a21/main.py
@@ -6,8 +6,8 @@ import pandas as pd
 from indicator_cci import CCI
 
 class TradingStrategy(Strategy):
-    def __init__(self):
-        self.tickers = ["TSLA"]
+    def __init__(self, ticker):
+        self.ticker = ticker
         self.data_list = []
 
     @property
@@ -16,25 +16,25 @@ class TradingStrategy(Strategy):
 
     @property
     def assets(self):
-        return self.tickers
+        return [self.ticker]
 
     @property
     def data(self):
         return self.data_list
 
     def run(self, data):
-        cci_fast = CCI("TSLA", data["ohlcv"], 26)
-        cci_medi = CCI("TSLA", data["ohlcv"], 127)
-        cci_slow = CCI("TSLA", data["ohlcv"], 132)
+        cci_fast = CCI(self.ticker, data["ohlcv"], 26)
+        cci_medi = CCI(self.ticker, data["ohlcv"], 127)
+        cci_slow = CCI(self.ticker, data["ohlcv"], 132)
 
         # Equivalent to warm-up period, if all values not ready just show allocation (aka do nothing)
         if len(cci_fast) == 0 or len(cci_medi) == 0 or len(cci_slow) == 0 :
-                return TargetAllocation({})
+            return TargetAllocation({})
 
         # Example CCI-based strategy: Enter when CCI(10) crosses above CCI(40)
         if cci_fast[-1] > 0 and cci_medi[-1] > 0 and cci_slow[-1] > 0:
-                allocation = 1  # Example fixed allocation
+            allocation = 1  # Example fixed allocation
         else:
-                allocation = 0  # No position
+            allocation = 0  # No position
 
-        return TargetAllocation({"TSLA": allocation})
+        return TargetAllocation({self.ticker: allocation})
