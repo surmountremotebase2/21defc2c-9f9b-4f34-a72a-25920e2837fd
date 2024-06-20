6fd7bd22018e20180868b80526f55b2a68890209
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-20 10:27:46 -0400

    User Ryan Cheng saved changes to multiple files files at 06/20/2024, 14:27:45 UTC time.

--- 8832b6c3-99fa-4c85-9946-5242ec521a21/main.py
+++ 8832b6c3-99fa-4c85-9946-5242ec521a21/main.py
@@ -3,31 +3,9 @@ from surmount.logging import log
 from surmount.data import Asset, InstitutionalOwnership
 import pandas_ta as ta
 import pandas as pd
+import indicator_cci
 
-def CCI(ticker, data, length):
-    '''Calculate the Commodity Channel Index (CCI) for the given ticker.
 
-    :param ticker: a string ticker
-    :param data: data as provided from the OHLCV data function
-    :param length: the window
-
-    :return: list with float CCI values
-    '''
-    high = [i[ticker]["high"] for i in data]
-    low = [i[ticker]["low"] for i in data]
-    close = [i[ticker]["close"] for i in data]
-
-    # DataFrame to hold high, low, and close values
-    df = pd.DataFrame({
-        'high': high,
-        'low': low,
-        'close': close
-    })
-
-    cci = ta.cci(df['high'], df['low'], df['close'], length=length)
-    if cci is None:
-        return None
-    return cci.tolist()
 
 class TradingStrategy(Strategy):
     def __init__(self):
