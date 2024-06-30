35ede71798627e23ed8e1f33fbc1d2e9e1cf5418
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-30 05:02:06 -0400

    User Ryan Cheng saved changes to main.py at 06/30/2024, 09:02:05 UTC time.

--- 6a0faf04-fc8d-4b9c-b7e4-d9b8dcb23419/main.py
+++ 6a0faf04-fc8d-4b9c-b7e4-d9b8dcb23419/main.py
@@ -1,67 +1,24 @@
 from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
 from surmount.logging import log
-from surmount.data import Asset, InstitutionalOwnership
-import pandas as pd
-import pandas_ta as ta
-
-tickers = ["QQQ"]
-
-def crossovers(ticker, data, length1, length2):
-    '''Calculate the crossover between two indicators for the given ticker.
-
-    :param ticker: a string ticker
-    :param data: data as provided from the OHLCV data function
-    :param length1: the window for the sma
-    :param length2: the window for the ema
-
-    :return: list with float values
-    '''
-    high = [i[ticker]["high"] for i in data]
-    low = [i[ticker]["low"] for i in data]
-    close = [i[ticker]["close"] for i in data]
-
-    # DataFrame to hold high, low, and close values
-    df = pd.DataFrame({
-        'high': high,
-        'low': low,
-        'close': close
-    })
-
-    indicator1 = ta.sma(df['high'], df['low'], df['close'], length=length1)
-    indicator2 = ta.ema(df['high'], df['low'], df['close'], length=length2)
-    
-    if indicator1 is None or indicator2 is None:
-        return None
-    return indicator1.tolist(), indicator2.tolist()
 
 class TradingStrategy(Strategy):
-    def __init__(self):
-        self.tickers = ["QQQ"]
-        self.data_list = []
+
+    @property
+    def assets(self):
+        return ["QQQ"]
 
     @property
     def interval(self):
         return "1hour"
 
-    @property
-    def assets(self):
-        return [self.tickers]
-
-    @property
-    def data(self):
-        return self.data_list
-
     def run(self, data):
-        crossover_values = crossovers(self.tickers, data["ohlcv"], 20, 14)
+        d = data["ohlcv"]
+        qqq_stake = 0
+        if len(d)>3 and "13:00" in d[-1]["QQQ"]["date"]:
+            v_shape = d[-2]["QQQ"]["close"]<d[-3]["QQQ"]["close"] and d[-1]["QQQ"]["close"]>d[-2]["QQQ"]["close"]
+            log(str(v_shape))
+            if v_shape:
+                qqq_stake = 1
 
-        # Equivalent to warm-up period, if all values not ready just show allocation (aka do nothing)
-        if crossover_values is None or len(crossover_values[0]) == 0 or len(crossover_values[1]) == 0:
-            return TargetAllocation({})
-
-        # Example CCI-based strategy: Enter when crossover_ema crosses above crossover_sma
-        if crossover_values[0][-1] > crossover_values[1][-1]:
-            allocation = 1  # Example fixed allocation
-        else:
-            allocation = 0  # No position
-
-        return TargetAllocation({self.tickers: allocation})
+        return TargetAllocation({"QQQ": qqq_stake})
