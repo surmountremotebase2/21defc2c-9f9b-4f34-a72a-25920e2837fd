62e3147c938368ac13d27894033d6f0696289d1e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-30 05:11:01 -0400

    User Ryan Cheng saved changes to multiple files files at 06/30/2024, 09:11:00 UTC time.

--- 1308d3ee-a835-42f5-969a-ce06efc08ce7/main.py
+++ 1308d3ee-a835-42f5-969a-ce06efc08ce7/main.py
@@ -1,39 +1,24 @@
-#Type code herefrom surmount.base_class import Strategy, TargetAllocation
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
 from surmount.logging import log
-from surmount.data import Asset, InstitutionalOwnership
-from indicator_crossovers import crossovers
-
-ticker = "SPY"
+from indicator_vshape import vshape
 
 class TradingStrategy(Strategy):
-    def __init__(self, ticker):
-        self.ticker = ticker
-        self.data_list = []
+
+    @property
+    def assets(self):
+        return ["QQQ"]
 
     @property
     def interval(self):
         return "1hour"
 
-    @property
-    def assets(self):
-        return [self.ticker]
-
-    @property
-    def data(self):
-        return self.data_list
-
     def run(self, data):
-        crossovers = crossovers(self.ticker, data["ohlcv"], 20)
-        crossover_ema = crossovers(self.ticker, data["ohlcv"], 14)
+        d = data["ohlcv"]
+        qqq_stake = 0
+        if len(d)>3 and "13:00" in d[-1]["QQQ"]["date"]:
+            v_shape = vshape(data)
+            if v_shape:
+                qqq_stake = 1
 
-        # Equivalent to warm-up period, if all values not ready just show allocation (aka do nothing)
-        if len(crossover_sma) == 0 or len(crossover_ema) == 0:
-            return TargetAllocation({})
-
-        # Example CCI-based strategy: Enter when crossover_ema crosses above crossover_sma
-        if crossover_ema[-1] > crossover_sma[-1]:
-            allocation = 1  # Example fixed allocation
-        else:
-            allocation = 0  # No position
-
-        return TargetAllocation({self.ticker: allocation})
+        return TargetAllocation({"QQQ": qqq_stake})
